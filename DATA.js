c2183c498a3daf47d2f57e848273d89495945c59
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-03 00:52:25 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "212a3e1f-8862-4455-a597-4da864acb023",
+    "home_feed": [
+        {
+            "id": "04c65fc3-b656-4ddf-be88-d5f39e9f9e9e",
+            "artist_name": "recent",
+            "home_carousel": [
+                {
+                    "id": "d4aea23d-3ce3-46c9-b5a8-a7364621897d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "db0a05c8-f5df-462a-9aca-6ad8a8a9ac97",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                },
+                {
+                    "id": "d7c151c0-925f-4d46-a867-27c5176d897e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "6ddf47b4-f2f8-4542-ba67-4313c6e6ef39",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "42121d50-4cd9-4d92-aabe-bb58fa35ecf5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                }
+            ],
+            "art_caption": "travel",
+            "art_description": "Speak piece different more. Them outside start start expert."
+        },
+        {
+            "id": "0b2a6f88-1c36-4e32-bfdd-9766f5f2319e",
+            "artist_name": "term",
+            "home_carousel": [
+                {
+                    "id": "90b21976-8ad4-45fa-8d41-ee2df818aef2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "916f3d59-92cd-4b7b-9898-4b7c59a42ff7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                },
+                {
+                    "id": "4b3e3ad8-dc55-4b4a-a868-71311b0e7028",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "fe2e62be-8569-441f-a4dc-46b0505ca5f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/143.png"
+                },
+                {
+                    "id": "f7b2f7e2-477c-4c16-b3cc-a40e4b51a36e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png"
+                }
+            ],
+            "art_caption": "second",
+            "art_description": "Foot age boy choose realize into fly."
+        },
+        {
+            "id": "2e1dbdd2-3711-4162-a2ce-46c3a2dbcbe5",
+            "artist_name": "color",
+            "home_carousel": [
+                {
+                    "id": "50b8ac01-2f6d-4481-aaa8-2f18c61e32cc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png"
+                },
+                {
+                    "id": "f5c3489a-9536-4c04-baec-ba0d90675e9a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "9494dfdb-78ae-4509-94ac-14972a6a7d33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png"
+                },
+                {
+                    "id": "9ff8cb6c-724d-4e18-915a-07f4f2a024f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png"
+                },
+                {
+                    "id": "20480eb4-bacb-4ec3-8d6e-a4b01d319f7e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                }
+            ],
+            "art_caption": "case",
+            "art_description": "Woman establish reach camera worry. Cell rich front. Write book picture in."
+        },
+        {
+            "id": "3dcac5be-d64f-422b-add9-09633fdb49b6",
+            "artist_name": "girl",
+            "home_carousel": [
+                {
+                    "id": "6f1d63e3-2044-4245-8061-f61c830f8cc4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                },
+                {
+                    "id": "e2e656bb-554a-46b1-a73e-c9a274d813a9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "0a908852-5a39-4caa-a7c5-4fc4d87c0859",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png"
+                },
+                {
+                    "id": "8f933579-3d87-49cb-b50f-f3d6de4aec75",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/325.png"
+                },
+                {
+                    "id": "d4167bc9-422d-463f-8fee-0fdc8301b33c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png"
+                }
+            ],
+            "art_caption": "rich",
+            "art_description": "Property instead edge but. Light try life treatment."
+        },
+        {
+            "id": "2cec48af-1cf3-4404-89bf-0d0e27a693ad",
+            "artist_name": "air",
+            "home_carousel": [
+                {
+                    "id": "58b8daff-56af-4e66-8530-b9e75f67081c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "65292a2c-38a5-47a7-91d1-ea63cf114fda",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "5af03bf6-6bf9-4acf-843a-35c395f2e56e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                },
+                {
+                    "id": "3e066f0e-72cd-4d8c-a108-353c7abf1601",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "9be171c6-fb4a-4926-94a3-89d313e048ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                }
+            ],
+            "art_caption": "but",
+            "art_description": "Four recent a. Point painting himself image political everybody place."
+        }
+    ]
+}
